610eb57399b0c05c4172915a5b4704eb8e804bd3
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-04-01 23:55:54 +0600

    feature product added

--- src/components/Offer/Offer.js
+++ src/components/Offer/Offer.js
@@ -2,16 +2,20 @@ import React from 'react';
 import './Offer.css'
 
 const Offer = (props) => {
-    const { name, img, price, description } = props.offer
+    const { name, img, price, feature1, feature2, feature3, feature4 } = props.offer
     return (
         <div className="col-md-6 ">
             <div className="cart p-2 m-3 p-4 bg text-center">
                 <img className='w-50 m-auto  rounded' src={img} alt="" />
                 <h4 className='py-3'>{name}</h4>
-                <p className='pb-3'>{description.slice(0, 180)}</p>
-                <div className='d-flex justify-content-around my-2'>
-                    <button type='button' className="btn btn-warning text-light " >Offer Price :$ {price}</button>
-                    <button type='button' className="btn btn-warning text-light " >Add Cart Now</button>
+                {/* <p className='pb-3'>{description.slice(0, 180)}</p> */}
+                <li>{feature1}</li>
+                <li>{feature2}</li>
+                <li>{feature3}</li>
+                <li>{feature4}</li>
+                <div className='d-flex justify-content-around my-4'>
+                    <button type='button' className="btn btn-warning " >Offer Price :$ {price}</button>
+                    <button type='button' className="btn btn-warning " >Add Cart Now</button>
                 </div>
             </div>
         </div>
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from '../../components/Banner/Banner';
 import Offers from '../../components/Offers/Offers';
+import Products from '../../components/Products/Products';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
             <Offers></Offers>
+            <Products></Products>
         </div>
     );
 };

76a31314a2381baaa56e2aea6b2de727e10659c3
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-04-02 23:57:32 +0600

    order page handle

--- src/pages/Orders/Orders.js
+++ src/pages/Orders/Orders.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import useCart from '../../hooks/useCart';
+import useProducts from '../../hooks/useProducts';
 
 const Orders = () => {
+    const [products, setProducts] = useProducts()
+    const [cart, setCart] = useCart(products)
     return (
         <div>
-            <h3>Orders page</h3>
+            <h3 className='mt-5 pt-5 text-center'>Order page</h3>
+            <div className="row">
+                <div className="col-lg-8">
+
+                </div>
+                <div className="col-lg-4">
+
+                </div>
+            </div>
         </div>
     );
 };
